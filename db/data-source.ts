8e37d9a0e48ad1e6df1801972656751fe2dae262
vanbogota <vanbog@mail.ru>
2025-08-24 19:11:08 +0300

    tests

--- db/data-source.ts
+++ db/data-source.ts
@@ -6,22 +6,14 @@ dotenv.config();
 
 export const PostgresDataSource = new DataSource({
   type: 'postgres',
-  host: process.env.POSTGRES_HOST || 'localhost',
-  port: Number(process.env.POSTGRES_PORT) || 5432,
-  username: process.env.POSTGRES_USER || 'postgres',
-  password: process.env.POSTGRES_PASSWORD || 'postgres',
-  database: process.env.POSTGRES_DB || 'app_auth_db',
+  host: 'postgres',
+  port: Number(process.env.PG_PORT) || 5432,
+  username: process.env.PG_USER,
+  password: process.env.PG_PASSWORD,
+  database: process.env.PG_DB,
   synchronize: false,
   logging: true,
-  entities: [__dirname + '../src/**/*.entity{.ts,.js}'],
-  migrations: [__dirname + 'migrations/*{.ts,.js}'],
+  entities: ['dist/**/*.entity{.js}'],
+  migrations: ['dist/db/migrations/*{.js}'],
   subscribers: [],
 });
-
-PostgresDataSource.initialize()
-  .then(() => {
-    console.log('Data Source has been initialized!');
-  })
-  .catch((error) =>
-    console.log('Error during Data Source initialization: ', error),
-  );
