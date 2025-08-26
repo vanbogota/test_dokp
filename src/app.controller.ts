314e1010e185bd4e9e0e1fee4c50bf2180410169
vanbogota <vanbog@mail.ru>
2025-08-26 10:51:32 +0300

    get me added

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,13 +1,19 @@
-import { Controller, Post, Req } from '@nestjs/common';
-import { AppService } from './app.service';
+import { Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
+import { UsersService } from './entities/users/users.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly userService: UsersService) {}
 
+  //for testing auth0
   @Post('me')
   login(@Req() req: Request) {
     return req.user;
+  }
+
+  @Get('me')
+  getProfile(@Param('id') id: string) {
+    return this.userService.findById(id);
   }
 }
