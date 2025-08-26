fdb04f056626d3248c9adf9e279b446dffb54b83
vanbogota <vanbog@mail.ru>
2025-08-26 10:54:35 +0300

    updated app controller

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,8 @@
 import { Controller, Get, Param, Post, Req } from '@nestjs/common';
 import { Request } from 'express';
 import { UsersService } from './entities/users/users.service';
+import { ApiNotFoundResponse, ApiOkResponse, ApiOperation } from '@nestjs/swagger';
+import { UserResponseDto } from './entities/users/dto/UserResponseDto';
 
 @Controller()
 export class AppController {
@@ -13,6 +15,9 @@ export class AppController {
   }
 
   @Get('me')
+  @ApiOperation({ summary: 'Get a user by ID' })
+  @ApiOkResponse({ type: UserResponseDto })
+  @ApiNotFoundResponse({ description: 'User not found' })
   getProfile(@Param('id') id: string) {
     return this.userService.findById(id);
   }
